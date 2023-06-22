5303d85cf54450cf082efa66fb894b7980ad0a26
chauphuoc <chauphuoc1996@gmail.com>
2023-06-22 10:53:30 +0700

    product detail

--- tokyolife/src/index.js
+++ tokyolife/src/index.js
@@ -7,6 +7,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { IntlProvider } from 'react-intl';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -14,7 +15,10 @@ document.title = 'N2D shop'
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <IntlProvider>
       <App />
+      </IntlProvider>
+     
     </BrowserRouter>
   </React.StrictMode>
 );
